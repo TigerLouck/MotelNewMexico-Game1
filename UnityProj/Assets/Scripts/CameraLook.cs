275b64855973514dd6ae675514c3d7d694c5d5d6
TigerLouck <tigerlouck@gmail.com>
2020-01-19 00:35:13 -0500

    Configured some project details, upgraded to 2019.2.18 because hub bad
    -Added Vehicle test scene
    -Began Scripting vehicle
    -Upgraded .NET compat level to 4.x due to missing system type issues in
    VS

---
+++ UnityProj/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+	float mouseX;
+	float mouseY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		mouseX += Input.GetAxisRaw("Mouse X");
+		mouseY += Input.GetAxisRaw("Mouse Y");
+		Debug.Log(Input.GetAxis("Roll"));
+
+		transform.rotation = transform.rotation * Quaternion.Euler(mouseY, mouseX, Input.GetAxis("Roll"));
+
+	}
+}
---
+++ UnityProj/Assets/Scripts/CarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+	public Transform CameraPivot;
+	public WheelCollider[] Wheels;
+	public WheelCollider[] SteeringWheels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Move the wheels
+        if ()
+    }
+}
