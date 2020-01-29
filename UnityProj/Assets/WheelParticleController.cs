a1fc11e9e81a4c32d195e2209e5a4150361af9b3
Tiger Louck (RIT Student) <txl9017@ad.rit.edu>
2020-01-29 14:50:21 -0500

    Wheel FX work

--- UnityProj/Assets/WheelParticleController.cs
+++ UnityProj/Assets/WheelParticleController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class WheelParticleController : MonoBehaviour
 {
+    ParticleSystem PSystem;
+    ParticleSystem.EmissionModule emitter;
+    WheelCollider Wheel;
+    WheelHit hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PSystem = GetComponent<ParticleSystem>();
+        emitter = PSystem.emission;
+        Wheel = transform.parent.GetComponent<WheelCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Wheel.GetGroundHit(out hit))
+        {
+            emitter.rateOverTime = (Mathf.Abs(hit.forwardSlip) + Mathf.Abs(hit.sidewaysSlip)) * 100;
+        } 
+        else
+        {
+            emitter.rateOverTime = 0;
+        }
     }
 }
